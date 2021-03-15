cb9cb1efc9102924185167946b0afff60053782b
kghunt20 <kghunt20@gmail.com>
2021-03-15 09:40:26 -0500

    Update ProjectCodes.jl

--- ProjectCodes.jl
+++ ProjectCodes.jl
@@ -2,55 +2,67 @@
 #0. Packages
 ################################################################################
 
-using Random, Statistics, Distributions, Optim, DelimitedFiles
+using Random, Statistics, Distributions, Optim, DelimitedFiles, StatsBase
 
 ################################################################################
 #1. Import data
 ################################################################################
 
-cd("Documents/Github/ECON751_Project")
+cd("C:\\Users\\Kevin\\Documents\\Github\\ECON751_Project")
 #Order of variables in data: id, age, lfp, x, wage, edu, lfp0, hinc
 #I deleted variable names from this file, so it is just numers.
-data = readdlm("data_age4554.txt");
+original_data = readdlm("data_age4554.txt");
 
 ################################################################################
 #2. Define model structure (Utility functions and wage)
 ################################################################################
 
-#σ_ϵ = parameters[1]
+#σ_ξ= parameters[1]
 #σ_η = parameters[2]
-#r_s = parameters[3]
-#r_k = parameters[4]
-#α1 = parameters[5]
-#α0 = parameters[6]
-#s = schooling of wife
-#k = experience of wife
+#r_0 = parameters[3]
+#r_1 = parameters[4]
+#r_2 = parameters[5]
+#r_3 = parameters[6]
+#α1 = parameters[7]
+#α2 = parameters[8]
+#s = schooling
+#k = experience
 
 #Calculate the deterministic part of the wage of the wife
 function log_wage_det(parameters, s, k)
 
-    r_s = parameters[3]
-    r_k = parameters[4]
+    r_0 = parameters[3]
+    r_1 = parameters[4]
+    r_2 = parameters[5]
+    r_3 = parameters[6]
 
-    r_s*s + r_k*k
+    r_0 + r_1*s + r_2*k
 
 end
 
 #Calculate the current period utility from working
-function c_U1(parameters, c_y, w, s, k)
+function U1(parameters, c_y, w, s, k)
 
     c_y + w
 
 end
 
 #Calculate the current period utility from not working
-function c_U0(parameters, c_y, w, s, k)
+function U0(parameters, c_y, w, s, k)
 
-    α1 = parameters[5]
-    α0 = parameters[6]
+    α1 = parameters[7]
+    α2 = parameters[8]
 
-    c_y + α1*c_y + α0
+    c_y + α2*c_y + α1
 
+end
+
+function Φ(x)
+    cdf(Normal(), x)
+end
+
+function subset(x; couples = couples)
+    x in couples
 end
 
 ################################################################################
@@ -61,34 +73,20 @@ end
 #and education level for the wife s.
 #T = Total number of periods the wife makes labor supply decisions.
 #k_start = Starting experience of the wife.
-#Δϵ = grid size for random component of wages
 
-function solve_model(parameters, y, s, k_start; T=T, β = β, Δϵ = Δϵ)
+function get_ξ_star(parameters, y, s, k_start; T=T, β = β)
 
-    σ_ϵ = parameters[1]
-
-    #For the distribution of the wage shock ϵ, choose lower (lb) and upper (ub)
-    #bounds for the grid of shocks.
-    ϵ_lb, ϵ_ub = round.(quantile.(Normal(0.0, σ_ϵ), [0.0001, 0.9999]),
-                        digits = Int(-log10(Δϵ)))
-
-    #Generate ϵ_grid
-    ϵ_grid = ϵ_lb:Δϵ:ϵ_ub
-    N_ϵ = length(ϵ_grid)
-    ϕ = pdf.(Normal(0.0, σ_ϵ), ϵ_grid)
+    σ_ξ = parameters[1]
 
     #Generate a grid of possible experience the wife could achieve
     k_grid = k_start:(k_start + T-1)
 
-    #Intialize model functions:
-    #Value function V(ϵ, t, k)
-    V = zeros(N_ϵ, T, T)
     #Expected value function EV(t, k)
     EV = zeros(T+1, T+1)
-    #Participation function P(ϵ, t, k)
-    P = zeros(N_ϵ, T, T)
-    #Participation cut-off ϵ^* (ϵ_start)
-    ϵ_star = zeros(T, T)
+    #Participation cut-off ϵ^* (ϵ_star)
+    ξ_star = zeros(T, T)
+
+    ξ_lb = quantile(Normal(0.0, σ_ξ), 0.0001)
 
     #Solve the model recursively
     for t = reverse(1:T), i_k = 1:t
@@ -96,38 +94,28 @@ function solve_model(parameters, y, s, k_start; T=T, β = β, Δϵ = Δϵ)
         k = k_grid[i_k]
 
         #Deterministic part of wife's wage
-        log_w_bar = log_wage_det(parameters, s, k)
+        c_log_wage_det = log_wage_det(parameters, s, k)
 
-        #Calculate ϵ_star, being careful to not take log of negative number :)
-        A = c_U0(parameters, y[t], log_w_bar, s, k) -
-            c_U1(parameters, y[t], log_w_bar, s, k) +
+        #Calculate ξ_star
+        A = U0(parameters, y[t], c_log_wage_det, s, k) -
+            U1(parameters, y[t], c_log_wage_det, s, k) +
             β*EV[t+1, i_k] - β*EV[t+1, i_k + 1]
         if A > 0
-            ϵ_star[t, i_k] = log(A) - log_w_bar
+            ξ_star[t, i_k] = log(A) - c_log_wage_det
         else
-            ϵ_star[t, i_k] = ϵ_lb
+            ξ_star[t, i_k] = ξ_lb
         end
 
-        #Solve for value and participation function given the ϵ shock.
-        for i_ϵ = 1:N_ϵ
-
-            w = exp(log_wage_det(parameters, s, k) + ϵ_grid[i_ϵ])
-            U1 = c_U1(parameters, y[t], w, s, k) + β*EV[t+1, i_k + 1]
-            U0 = c_U0(parameters, y[t], w, s, k) + β*EV[t+1, i_k]
-
-            P[i_ϵ, t, i_k] = (U1 > U0)
-            V[i_ϵ, t, i_k] = maximum([U1 U0])
-
-        end
-
-        #Calculate expected value function using trapezoid rule.
-        EV[t, i_k] = Δϵ*(sum(V[:, t, i_k] .* ϕ) - 0.5*(V[1,t,i_k]*ϕ[1] +
-                     V[N_ϵ,t,i_k]*ϕ[N_ϵ]))
+        EV[t, i_k] = (y[t] + β*EV[t+1, i_k + 1] ) * (1-Φ(ξ_star[t,i_k]/σ_ξ)) +
+                     exp(c_log_wage_det)*exp(0.5*σ_ξ^2)*
+                        (1-Φ((ξ_star[t,i_k]-σ_ξ^2)/σ_ξ)) +
+                    (U0(parameters, y[t], exp(c_log_wage_det), s, k) + β*EV[t+1,i_k])*
+                        Φ(ξ_star[t, i_k]/σ_ξ) #Is there a typo in notes here?
 
     end
 
     #Deliverables
-    ϵ_grid, P, ϵ_star
+    ξ_star
 
 end
 
@@ -141,18 +129,14 @@ end
 
 function likelihood(parameters; data = data, T = T, β = β)
 
-    σ_ϵ = parameters[1]
+    σ_ξ = parameters[1]
     σ_η = parameters[2]
-    r_s = parameters[3]
-    r_k = parameters[4]
-    α1 = parameters[5]
-    α0 = parameters[6]
 
     LL = 0.0
 
     #Relevant parameters for likelihood function
-    σ_u = (σ_ϵ^2 + σ_η^2)^0.5
-    ρ = σ_ϵ/σ_u
+    σ_u = (σ_ξ^2 + σ_η^2)^0.5
+    ρ = σ_ξ/σ_u
 
 
     for id in unique(data[:,1])
@@ -164,10 +148,9 @@ function likelihood(parameters; data = data, T = T, β = β)
         data_s = data_now[1,6]
         data_P = data_now[:,3]
         k_start = Int(data_now[1,4])
-        k_grid = k_start:(k_start + T - 1)
+        data_k = data_now[:,4]
 
-
-        ϵ_grid, P, ϵ_star = solve_model(parameters, data_y, data_s, k_start)
+        ξ_star = get_ξ_star(parameters, data_y, data_s, k_start)
 
         i_k = 1
 
@@ -177,10 +160,10 @@ function likelihood(parameters; data = data, T = T, β = β)
             if data_P[t] == 1
 
                 u = log(data_wO[t]) -
-                    log_wage_det(parameters, data_s, k_grid[i_k])
-                A = (ϵ_star[t, i_k] - ρ*σ_ϵ/σ_u*u)/(σ_ϵ*(1-ρ^2)^0.5)
+                    log_wage_det(parameters, data_s, data_k[t])
+                A = (ξ_star[t, i_k] - ρ*σ_ξ/σ_u*u)/(σ_ξ*(1-ρ^2)^0.5)
                 B = pdf(Normal(), u/σ_u)/σ_u
-                LL -= log( (1.0-cdf(Normal(), A))*B )
+                LL -= log( (1.0-cdf(Normal(), A))*B)
 
                 #Increase experience for next period
                 i_k += 1
@@ -188,7 +171,7 @@ function likelihood(parameters; data = data, T = T, β = β)
             #Update LL if wife doesn't work.
             elseif data_P[t] == 0
 
-                LL -= log(cdf(Normal(), ϵ_star[t, i_k]/σ_ϵ))
+                LL -= log(cdf(Normal(), ξ_star[t, i_k]/σ_ξ))
 
             end
 
@@ -196,10 +179,7 @@ function likelihood(parameters; data = data, T = T, β = β)
 
     end
 
-    #Show the LL while optimize iterates so you know it is doing something
-    #@show LL
-
-    LL
+    @show LL
 
 end
 
@@ -208,25 +188,26 @@ end
 ################################################################################
 
 β = 0.95
-#Δϵ must be a multiple of 10.
-Δϵ = 0.1
 #Number of periods the wife makes labor supply decisions.
 T = 20
 
 #Initial guess
-x0 = [2.22, 0.3, 0.7, 0.00, 0.44, 0.34]
+x0 =  [0.5987529235445073, 0.19021365175159843, 9.33874450407129,
+       0.047565980905554126, 0.007830603645591645, -32.41353995455251,
+       23912.216292202516, 0.008811804383663021]
 
-#Start with only 10% of data to get a good initial guess quickly
-store_data = data
-data = data[1:2000,:]
-res1 = optimize(likelihood, x0, time_limit = 3000,
-                rel_tol = 0.001, abs_tol = 0.001)
+#Start with only 250 couples of data to get a good initial guess quickly
+#couples = sample(unique(store_data[:,1]), 100,  replace = false)
+couples = sample(unique(data[:,1]), 250, replace = false)
+data = original_data[subset.(original_data[:,1]),:]
+res1 = optimize(likelihood, x0, iterations)
 
-#Now use half of data
-data = store_data[1:10000,:]
-x0 = res.minimizer
-res2 = optimize(likelihood, x0, time_limit = 3000,
-                rel_tol = 0.001, abs_tol = 0.001)
+x0 = res1.minimizer
+
+#Now use all the data
+data = store_data
+res1 = optimize(likelihood, x0)
+
 
 
 ################################################################################
@@ -241,33 +222,102 @@ res2 = optimize(likelihood, x0, time_limit = 3000,
 ################################################################################
 #*******************************************************************************
 ################################################################################
-#II.  Model Fit
+#II.  Model Fit: In simulating data from your model, you should do 20
+#     replications for each couple.  Then you should compare the simulations to
+#     the actual data in terms of the following statistics:
 ################################################################################
 #*******************************************************************************
 ################################################################################
 
+function simulate_data(parameters, N, y, s, k_start; T = T, β = β)
+
+    σ_ξ = parameters[1]
+    σ_η = parameters[2]
+
+    ξ_star = get_ξ_star(parameters, y, s, k_start)
+
+    observations = zeros(T*N, 8)
+
+    #Order of variables in data: id, age, lfp, x, wage, edu, lfp0, hinc
+    i = 1
+    for n = 1:N
+
+        i_k = 1
+        k = k_start
+        wO = 0.0
+        k_prime = k
+        P = 0.0
+
+        for t = 1:T
+
+            ξ = randn()*σ_ξ
+
+            if ξ > ξ_star[t, i_k]
+                P = 1
+                wO = exp(log_wage_det(parameters, s, k_start + i_k -1) +
+                         randn()*σ_η)
+                k_prime = k+1
+                i_k = i_k + 1
+            else
+                P = 0
+                wO = -9.0
+            end
+
+            observations[i,:] = [n, t, P, k, wO, s, 1, y[t]]
+            k = k_prime
+            i+=1
+
+        end
+
+    end
+
+    observations
+
+end
+
 ################################################################################
-#1. Simulate 20 wives for each real wife in the data.
+#1. The average number of period working over the 10 years overall and by
+#   whether or not the woman has less than 12 years of schooling, exactly 12,
+#   13-15 and 16+.
 ################################################################################
 
 
-################################################################################
-#2. Function that calculates average number of periods worked over lifetime for
-#   subsamples in both data and simulations:
-#   i. Everyone
-#   ii. Less than 12 years education
-#   iii. 12 years eduation
-#   iv. 13-15 years education
-#   v. 16+ years education
-################################################################################
+
+
+
+
 
 ################################################################################
-#3. Function that calculates fraction of women working at each age in both data
-#   and simulations.
+#2. The fraction of women working at each age.
 ################################################################################
 
+
+
+
+
+
+
+
+
+
+
 ################################################################################
-#4. ....
+#4. The fraction of women working by work experience levels (a) 10 years or
+#   less, (b) 11-20, (c) 21+.
+################################################################################
+
+
+
+
+
+
+
+
+
+
+################################################################################
+#5. The 2x2 table of labor force participation by lagged labor force
+#   participation.
 ################################################################################
 
 
@@ -279,4 +329,62 @@ res2 = optimize(likelihood, x0, time_limit = 3000,
 #III.  Counterfactual Analysis.
 ################################################################################
 #*******************************************************************************
+################################################################################
+
+################################################################################
+#1. Simulate data for participation and (accepted) wages for ages 55 to 64.
+#   How many years, on average, will women work between those ages, overall and
+#   by education?
+################################################################################
+
+
+
+
+
+
+
+
+
+
+################################################################################
+#2. Assume that the government introduces a flat income tax on total earnings
+#   (husband + wife) of 10 percent. Assume that couples report the woman’s
+#   earnings accurately to the IRS, although the wage is misreported in our
+#   data.  You should therefore use your estimate of the true wage for this
+#   exercise.
+#        (a) What will happen to the average number of years worked between
+#            the ages 45-54?   What is the total revenue the IRS will collect?
+#        (b)  Do the same for ages 55-64.
+################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################################
+#3. Suppose the tax schedule is instead progressive.  The couple pays a 10
+#   percent tax on the first 50,000 and a 20 percent tax on anything above that.
+#   Assume that the couples reported the woman’s earnings accurately to the IRS,
+#   although the wage is misreported in our data. You should therefore use your
+#   estimate of the true wage for this exercise.a) What will happen to the
+#   average number of years worked between the ages 45-54?   What is the total
+#   revenue the IRS will collect? (b)  Do the same for ages 55-64.
 ################################################################################
